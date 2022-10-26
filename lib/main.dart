eea947db18fbaa0192977ff381f5560c9c8ef3a5
Hanief-Aditiawan <nayazila123@gmail.com>
2022-10-26 15:35:20 +0700

    lastfinish

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: const MyHomePage(title: ''),
+      home: const MyHomePage(title: 'Hanief Aditiawan'),
     );
   }
 }
